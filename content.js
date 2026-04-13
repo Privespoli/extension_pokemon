ed9d9acd543209642c166153b784559c15e1f5d3
Priscila Nunes <priscilavespoli92@gmail.com>
2026-04-13 17:37:07 +0200

    ok

--- content.js
+++ content.js
@@ -4,20 +4,22 @@ document.addEventListener('click', (e) => {
 
   chrome.runtime.sendMessage({ action: "getRandomPokemon" }, (response) => {
     if (response && response.imgUrl) {
-      const img = new Image(); // Cria um objeto de imagem primeiro
+      const img = new Image(); 
       img.src = response.imgUrl;
       img.classList.add('poke-click');
       
-      // Centraliza melhor (ajustado para os 100px do novo CSS)
+      // Estilo para centralizar (ajustando metade da largura/altura definida no CSS)
       img.style.left = `${posX - 50}px`;
       img.style.top = `${posY - 50}px`;
       
-      // Só coloca na tela quando a imagem terminar de baixar
+      // Só adiciona ao corpo da página quando a imagem carregar
       img.onload = () => {
         document.body.appendChild(img);
+        
+        // Remove o Pokémon após 2 segundos com um efeito de sumiço
         setTimeout(() => {
           img.style.opacity = '0';
-          setTimeout(() => img.remove(), 400);
+          setTimeout(() => img.remove(), 500); // Espera o fade-out acabar
         }, 2000);
       };
     }
